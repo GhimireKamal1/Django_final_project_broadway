a2e1105bc82488963b6df1493b612a76cb804838
GhimireKamal1 <kamalghimire689@gmail.com>
2024-06-01 12:21:49 +0545

    initial commit

---
+++ config/wsgi.py
@@ -0,0 +1,8 @@
+
+import os
+
+from django.core.wsgi import get_wsgi_application # type: ignore
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
